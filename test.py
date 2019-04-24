29d9947daa528d049a7a3b7c6ed830ca31dd1b83
MatevzKopac <49947077+MatevzKopac@users.noreply.github.com>
2019-04-24 15:19:09 +0200

    nova sprememba tega

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 # napiši program, ki izpiše prvih 200 praštevil
 
-def je_praštevilo(n):
+def je_praštevilorarh(n):
     for i in range(2, n):
         if n % 1 == 0:
             return False
         return True
 
-for x in range(2, 201):
+for x in range(2, 244401):
     if je_praštevilo(x):
         print(x)
